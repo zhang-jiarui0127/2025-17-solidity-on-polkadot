09d8209959279a5bdecc9fe4b79bf89940375967
root <root@tjdm-isa-ai-p800node09.tjdm.baidu.com>
2025-03-24 02:53:13 +0000

    Update

--- homework-2/1762/ERC20.sol
+++ homework-2/1762/ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
@@ -8,85 +8,69 @@ interface IERC20 {
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-    // ERC20 events
+    
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract MyERC20Token is IERC20 {
-    // Token details
+contract ERC20 is IERC20 {
     string public name;
     string public symbol;
     uint8 public decimals;
-    
-    // Internal total supply variable
     uint256 private _totalSupply;
     
-    // Mappings for account balances and allowances
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
-
-    // Constructor initializes the token with a name, symbol and an initial supply.
-    // All initial tokens are assigned to the contract deployer.
-    constructor(string memory _name, string memory _symbol, uint256 initialSupply) {
+    
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 initialSupply) {
         name = _name;
         symbol = _symbol;
-        decimals = 18;  // Common default for ERC20 tokens
-        _totalSupply = initialSupply * 10 ** uint256(decimals);
+        decimals = _decimals;
+        _totalSupply = initialSupply * (10 ** uint256(decimals));
         _balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
-
-    // Returns the total token supply.
-    function totalSupply() public view override returns (uint256) {
+    
+    function totalSupply() external view override returns (uint256) {
         return _totalSupply;
     }
     
-    // Returns the token balance of the specified address.
-    function balanceOf(address account) public view override returns (uint256) {
+    function balanceOf(address account) external view override returns (uint256) {
         return _balances[account];
     }
     
-    // Transfers tokens from the caller to the recipient.
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-        require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
-
-        // Update balances
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        require(recipient != address(0), "Transfer to zero address");
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+        
         _balances[msg.sender] -= amount;
         _balances[recipient] += amount;
-
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
     
-    // Returns the remaining number of tokens that spender is allowed to spend on behalf of owner.
-    function allowance(address owner, address spender) public view override returns (uint256) {
+    function allowance(address owner, address spender) external view override returns (uint256) {
         return _allowances[owner][spender];
     }
     
-    // Approves the spender to transfer up to amount tokens on behalf of the caller.
-    function approve(address spender, uint256 amount) public override returns (bool) {
-        require(spender != address(0), "ERC20: approve to the zero address");
-
+    function approve(address spender, uint256 amount) external override returns (bool) {
+        require(spender != address(0), "Approve to zero address");
+        
         _allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
     
-    // Transfers tokens from sender to recipient using the allowance mechanism.
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
-        require(_allowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
-
-        // Update balances and allowance
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        require(sender != address(0), "Transfer from zero address");
+        require(recipient != address(0), "Transfer to zero address");
+        require(_balances[sender] >= amount, "Insufficient balance");
+        require(_allowances[sender][msg.sender] >= amount, "Allowance exceeded");
+        
         _balances[sender] -= amount;
         _balances[recipient] += amount;
         _allowances[sender][msg.sender] -= amount;
-
+        
         emit Transfer(sender, recipient, amount);
         return true;
     }
