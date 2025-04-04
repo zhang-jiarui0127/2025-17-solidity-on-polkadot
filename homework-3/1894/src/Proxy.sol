ac010fc322fc8c765c620cb94d2030f202e76caf
yangyuguang <2556885696@qq.com>
2025-04-04 21:52:24 +0800

    feat: 第三天作业

--- homework-3/1894/src/Proxy.sol
+++ homework-3/1894/src/Proxy.sol
@@ -3,22 +3,19 @@ pragma solidity ^0.8.20;
 
 contract Proxy {
     address public logicContractAddress;
-    uint256 public counter; // State is stored here
+    uint256 public counter;
 
     constructor(address _logicContractAddress) {
         logicContractAddress = _logicContractAddress;
     }
 
     function incrementProxy() public returns (bool success) {
-        // Get the function selector for Logic.increment()
         bytes4 selector = bytes4(keccak256("increment()"));
 
-        // Use delegatecall to execute Logic.increment() in the context of Proxy
         (success, ) = logicContractAddress.delegatecall(abi.encodeWithSelector(selector));
         require(success, "Delegatecall failed");
     }
 
-    // Optional: Function to update the logic contract address
     function setLogicContractAddress(address _newLogicContractAddress) public {
         logicContractAddress = _newLogicContractAddress;
     }
