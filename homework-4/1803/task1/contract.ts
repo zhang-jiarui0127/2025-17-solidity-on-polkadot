604db6fb357f534561e80b12c9004140e992d310
BTBMan <1498118157@qq.com>
2025-04-11 06:23:11 +0800

    wip: use vime
    Signed-off-by: BTBMan <1498118157@qq.com>

--- homework-4/1803/task1/contract.ts
+++ homework-4/1803/task1/contract.ts
@@ -1,36 +1,45 @@
-import { Contract, ethers } from 'ethers'
-import type { Account, Hex, PublicClient, WalletClient } from 'viem'
+import { Contract, ethers } from 'ethers';
+import type { Account, Hex, PublicClient, WalletClient } from 'viem';
 
 /**
  * 使用ethers部署合约
- * @param abi 
- * @param bytecode 
- * @param wallet 
- * @returns 
+ * @param abi
+ * @param bytecode
+ * @param wallet
+ * @returns
  */
-export async function deployContractByEthers(abi: any[], bytecode: string, wallet: ethers.Wallet) {
-  const factory = new ethers.ContractFactory(abi, bytecode, wallet)
-  const contract = await factory.deploy()
-  await contract.waitForDeployment()
-  const contractAddress = contract.target.toString()
-  return contractAddress
+export async function deployContractByEthers(
+  abi: any[],
+  bytecode: string,
+  wallet: ethers.Wallet,
+) {
+  const factory = new ethers.ContractFactory(abi, bytecode, wallet);
+  const contract = await factory.deploy();
+  await contract.waitForDeployment();
+  const contractAddress = contract.target.toString();
+  return contractAddress;
 }
 
 /**
  * 使用viem部署合约
- * @param abi 
- * @param bytecode 
- * @param wallet 
- * @returns 
+ * @param abi
+ * @param bytecode
+ * @param wallet
+ * @returns
  */
-export async function deployContractByViem(abi: any[], bytecode: Hex, publicClient: PublicClient, walleClient: WalletClient) {
+export async function deployContractByViem(
+  abi: any[],
+  bytecode: Hex,
+  publicClient: PublicClient,
+  walleClient: WalletClient,
+) {
   const hash = await walleClient.deployContract({
     abi,
     account: walleClient.account as Account,
     bytecode,
     chain: walleClient.chain,
-    args: []
-  })
-  const receipt = await publicClient.waitForTransactionReceipt({ hash })
-  return receipt.contractAddress as `0x${string}`
+    args: [],
+  });
+  const receipt = await publicClient.waitForTransactionReceipt({ hash });
+  return receipt.contractAddress as `0x${string}`;
 }
